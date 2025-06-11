c60b3508471d17f160060c575714082a0b87f173
Mohammednur <mamebb2023@gmail.com>
2025-06-11 11:27:46 +0300

    Update metadata in layout and enhance footer links; add favicon and banner images

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,61 @@ import { Toaster } from "react-hot-toast";
 import { Lexend_Deca } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: "SanAI | Your Personal AI Doctor",
+  metadataBase: new URL("https://sanai.live"),
+  title: {
+    default: "SanAI | Your Personal AI Doctor",
+    template: `%s - SanAI`,
+  },
   description:
     "SanAI is your personal AI doctor, providing personalized health insights and recommendations.",
+  keywords: [
+    "AI",
+    "artificial intelligence",
+    "personal doctor",
+    "health",
+    "recommendations",
+    "AI assistant",
+    "health insights",
+    "research",
+    "image analysis",
+  ],
+  authors: [{ name: " Team", url: "https://sanai.live" }],
+  category: "Technology",
+  applicationName: "SanAI",
+  formatDetection: {
+    telephone: false,
+    email: false,
+    address: false,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SanAI | Your Personal AI Doctor",
+
+    description:
+      "SanAI is your personal AI doctor, providing personalized health insights and recommendations.",
+    creator: "@ai",
+    site: "@ai",
+    images: [
+      {
+        url: "/banner.jpg",
+        width: 1200,
+        height: 630,
+        alt: "SanAI",
+      },
+    ],
+  },
+  icons: {
+    icon: [{ url: "/favicon.png", sizes: "any" }],
+    shortcut: "/favicon.png",
+  },
 };
 
 const font = Lexend_Deca({
